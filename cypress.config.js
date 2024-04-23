c59017531a6f4d7ce477bd457fb47924c7437349
FERNANDO JOSÉ PEREIRA DE SOUSA <FERNANDO0156@DIGITALCOLLEGE.LOCAL>
2024-04-22 21:51:45 -0300

    Cypress Config

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+export default defineConfig ({
+    e2e: {
+        baseUrl: 'http://127.0.0.1:5000',
+        setupNodeEvents(on, config){
+            //implement node event listeners here
+        },
+    },
+});
